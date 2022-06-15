ef1050b2da56469e1aedac71fa340833b0d5e65b
Christian <106610236+cmlooff@users.noreply.github.com>
2022-06-15 15:39:25 -0400

    Add remove task action at [X] icon

--- app.js
+++ app.js
@@ -10,29 +10,33 @@ loadEventListeners();
 
 // Load all event listeners function
 function loadEventListeners() {
-  //* Add task event
-  form.addEventListener('submit', addTask);
+  form.addEventListener('submit', addTask); //// Add task event
+  taskList.addEventListener('click', removeTask); //// Remove task event
 }
 
-function addTask(e) {
+function addTask(e) { //* Add task function
   if (taskInput.value === '') { // Checking user input field
     alert('Add a task');
   }
-
-  const li = document.createElement('li'); //* Create li element
-  li.className = 'collection-item'; // Add class
-  //* Create text node and append to li
-  li.appendChild(document.createTextNode(taskInput.value));
-  //* Create new link element for clearing tasks
-  const link = document.createElement('a');
-  //* Add class
-  link.className = 'delete-item secondary-content'; //* For materialize CSS we need secondary-content to put the link on the right of the content
-  link.innerHTML = '<i class = "fa fa-remove"></i>'; //* Add icon HTL
+  const li = document.createElement('li'); // Create li element. "Task"
+  li.className = 'collection-item'; // Add class name to created li
+  li.appendChild(document.createTextNode(taskInput.value)); // Create text node & append to li
+  const link = document.createElement('a'); //* Create new link element for clearing tasks
+  link.className = 'delete-item secondary-content'; //* Add class name. For materialize CSS we need secondary-content to put the link on the right of the content
+  link.innerHTML = '<i class = "fa fa-remove"></i>'; //* Add icon [X] HTML 
   li.appendChild(link) //* Append link to li
 
-  taskList.appendChild(li) //* Append li to ul
+  taskList.appendChild(li) // Append li to ul [collection]
 
-  taskInput.value = ''; //* Clear input
+  taskInput.value = ''; // Clear input
 
   e.preventDefault(); // Prevent default behavior 
+}
+
+function removeTask(e) { //// Remove task function
+  // If function to target the [X] icon for 'click'
+  if (e.target.parentElement.classList.contains('delete-item')) {
+
+  }
+
 }
